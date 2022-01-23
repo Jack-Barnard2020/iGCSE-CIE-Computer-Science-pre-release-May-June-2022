538e3e6f879aa7041f05006e7e73337ffda511bf
Jack-Barnard2020 <63458681+Jack-Barnard2020@users.noreply.github.com>
2022-01-23 17:54:25 +0000

    Update Pre_relese_task2.py

--- Pre_relese_task2.py
+++ Pre_relese_task2.py
@@ -1,151 +1,210 @@
-import datetime   #Import datetime to get current date without user input
-import csv      #Import csv to save memeber infomation in spreadhseet 
-
-
-date = str()
-current_date = str()
-full_date = str()
-
-full_name = str()
-full_name_correct = str()
-
-volunteer = str()
-volunteer_type = int()
-volunteer_type_correct = str()
-volunteer_type_array = ["entrance gate", "gift shop", "painting and decorating"]
-
-pay_today = str()
-payed = str()
-
-sponsor = str()
-sponsor_name = str()
-sponsor_message = str()
-sponsor_message_correct = str()
+import datetime #gets the current year to calculate if membership is expired
+import csv #enables us to read the csv data base
 
 
 
-def get_current_date():
+subject = "" #subject is the varible used to compare to the set values that would put a member on that list
+
+full_date = "" 
+current_date = ""
+
+element = [] #element is a list, that stores the split up user join date
+single_element = int() #stores the year of which the user joined
+current_year = int() #stores the current date
+
+members = []
+members_volunteers = []
+members_volunteers_entrancegate = []
+members_volunteers_giftshop = []
+members_volunteers_painting = []
+members_expired = []
+members_unpaid = []
+#these are all the lists which we will append values for
+#they are labled to clearly identfiy which list is for what
+#searhces for what task2 asks for
+
+list_of_lists = ["members", "members_volunteers", "members_volunteers_entrancegate", "members_volunteers_giftshop", "members_volunteers_painting", "members_expired", "members_unpaid"]
+list_to_search = int()
+to_return = str()
+#these are for the last subroutine in this file called "member_search" it enables us to use a for loop to output what each interger will lookup
+
+again = str()
+#sets i to empty so that the prgram loops
+
+def members_list_create():
+    filename = open('member_records.csv', 'r') #this sets the file to read only so we cant edit the file inside this program
+    file = csv.DictReader(filename) #this sets the file to read as a dictionary
+    members = []
+    for col in file: 
+        members.append(col['Name'])
+    print(members)
+#this adds evrything under the name collum and adds it to the list called members
+#we must set the list to have nothing it before runnning to stop haveing any double entries    
+
+def members_volunteers_list_create():
+    filename = open('member_records.csv', 'r') #this sets the file to read only so we cant edit the file inside this program
+    file = csv.DictReader(filename) #this sets the file to read as a dictionary
+    members_volunteers = []
+    for col in file:
+        subject = col['Volunteer']
+        if subject == "no":
+            pass
+        else:
+            subject = col['Name']
+            members_volunteers.append(subject)
+    print(members_volunteers)
+#this code checks if somone is not a volunter
+#then addes everyone else to the list
+#using the smae colum value becuase it is the same row
+#we must set the list to have nothing it before runnning to stop haveing any double entries    
+
+def members_volunteers_entrancegate_list_create():
+    filename = open('member_records.csv', 'r') #this sets the file to read only so we cant edit the file inside this program
+    file = csv.DictReader(filename) #this sets the file to read as a dictionary
+    members_volunteers_entrancegate = []
+    for col in file:
+        subject = col['Volunteer']
+        if subject == "entrance gate":
+            subject = col['Name']
+            members_volunteers_entrancegate.append(subject)
+        else:
+            pass
+    print(members_volunteers_entrancegate)
+#this cheks if someone volunters at the entrance gate
+#it passes over everyone else
+#using the smae colum value becuase it is the same row
+#we must set the list to have nothing it before runnning to stop haveing any double entries  
+
+def members_volunteers_giftshio_list_create():
+    filename = open('member_records.csv', 'r') #this sets the file to read only so we cant edit the file inside this program
+    file = csv.DictReader(filename) #this sets the file to read as a dictionary
+    members_volunteers_giftshop = []
+    for col in file:
+        subject = col['Volunteer']
+        if subject == "gift shop":
+            subject = col['Name']
+            members_volunteers_giftshop.append(subject)
+        else:
+            pass
+    print(members_volunteers_giftshop)
+#this checks if somone volunteers at the gift shop
+#it passes over everyone else
+#using the smae colum value becuase it is the same row
+#we must set the list to have nothing it before runnning to stop haveing any double entries
+
+def members_volunteers_painting_list_create():
+    filename = open('member_records.csv', 'r') #this sets the file to read only so we cant edit the file inside this program
+    file = csv.DictReader(filename) #this sets the file to read as a dictionary
+    members_volunteers_painting = []
+    for col in file:
+        subject = col['Volunteer']
+        if subject == "painting and decorating":
+            subject = col['Name']
+            members_volunteers_painting.append(subject)
+        else:
+            pass
+    print(members_volunteers_painting)
+#this checks if somone volunteers by painting and deocrating
+#it passes over everyone else
+#using the smae colum value becuase it is the same row
+#we must set the list to have nothing it before runnning to stop haveing any double entries
+
+def get_current_year():
+    filename = open('member_records.csv', 'r') #this sets the file to read only so we cant edit the file inside this program
+    file = csv.DictReader(filename) #this sets the file to read as a dictionary
     full_date = datetime.datetime.now()
     current_date = full_date.strftime("%x")
+    current_date = current_date.split('/')
+    current_date = int(current_date[2])
     return current_date
-    #gets full current date and then uses modifier to change it to short 6 digit date
-    #then returns the value to be called at sing up
+#this code uses the module datetime, imported at the begging of the file
+#it then modfies the date to a six digit date in the format mm/dd/yy
+#it tben splits the date and takes the last object in the list which is the year and makes it an interger so it can be used in a calulation 
 
-def fullname_sing_up():
-    full_name_correct = "i"
-    while full_name_correct != "y":
-        full_name = str(input("Please input your full name? "))
-        full_name_correct = str(input(f"Is {full_name} your correct full name: y/n ? ")).lower()
-        while full_name_correct != "y" and full_name_correct != "n":
+def members_expired_list_create():
+    filename = open('member_records.csv', 'r') #this sets the file to read only so we cant edit the file inside this program
+    file = csv.DictReader(filename) #this sets the file to read as a dictionary
+    members_expired = []
+    current_year = get_current_year()
+    for col in file:
+        subject = col['Date']
+        subject = subject.split('/')
+        subject = int(subject[2])
+        test = current_year - subject
+        if test >= 1:
+            subject = col['Name']
+            members_expired.append(subject)
+        else:
+            pass
+    print(members_expired)
+#this code uses the subroutine get_currrent_year
+#it takes the current year and subtracts the join date form it, if it is grater than or equeal to 1 it adds the user to the list
+#it passes over everyone else
+#using the smae colum value becuase it is the same row
+#we must set the list to have nothing it before runnning to stop haveing any double entries
+
+
+def members_unpaid_list_create():
+    filename = open('member_records.csv', 'r') #this sets the file to read only so we cant edit the file inside this program
+    file = csv.DictReader(filename) #this sets the file to read as a dictionary
+    members_unpaid = []
+    for col in file:
+        subject = col['Payed']
+        if subject == "yes":
+            pass
+        else:
+            subject = col['Name']
+            members_unpaid.append(subject)
+    print(members_unpaid)
+#this code looks to see if the memeber has payed, and then adds them to the list
+#it passes over everyone else
+#using the smae colum value becuase it is the same row
+#we must set the list to have nothing it before runnning to stop haveing any double entries
+
+def search_members():
+    print("************************************")
+    print("")
+    print("           Member Search")
+    print("")
+    print("************************************")
+    lists = -1
+    for i in list_of_lists:
+        lists = lists + 1
+        print(lists, "=" ,list_of_lists[lists])
+    again = "y"
+    while again == "y":
+        list_to_search = int(input("Input number of list to look up? "))
+        while list_to_search > 6:
             print("ERROR")
-            print(f"{full_name_correct} is not a valid input!")
-            full_name_correct = str(input(f"Is {full_name} your correct full name: y/n ? ")).lower()
-    return full_name
-    #Asks for users full name
-    #Validates if it is correct and allows user to edit
-    #Allows invalid inputs
-    #returns full name
+            print(f"{list_to_search} is not a valid input!")
+            list_to_search = int(input("Input number of list to look up? "))
+        if list_to_search == 0:
+            members_list_create()
+        elif list_to_search == 1:
+            members_volunteers_list_create()
+        elif list_to_search == 2:
+            members_volunteers_entrancegate_list_create()
+        elif list_to_search == 3:
+            members_volunteers_giftshio_list_create()
+        elif list_to_search == 4:
+            members_volunteers_painting_list_create()
+        elif list_to_search == 5:
+            members_expired_list_create()
+        elif list_to_search == 6:
+            members_unpaid_list_create()
+        else:
+            print("Unkonw ERROR")
+            search_members()
+        again = input("Would you like to search for a new lit of members: y/n ? ").lower()
+        while again != "y" and again != "n":
+            print("ERROR")
+            print(f"{again}, is not a valid input!")
+            again = input("Would you like to search for a new lit of members: y/n ? ").lower()
+    quit()
+#this is the final bit of code, it prints all the values assinged to the lists
+#once the value is slected it runs the selected subroutine
+#the vlaue is outputted
+#this repeates until the user desides for the prgoram to stop
+#it also allows the user to put in incorrect values and will repetedly ask the user for correct inputs until given
 
-def volunteer_sing_up():
-    volunteer = str(input("Would you like to be a volunteer: y/n ? ")).lower()
-    while volunteer != "y" and volunteer != "n":
-        print("ERROR")
-        print(f"{volunteer} is not a valid input!")
-        volunteer = str(input("Would you like to be a volunteer: y/n ? ")).lower()
-    if volunteer == "n":
-        volunteer = "no"
-    elif volunteer == "y":
-        volunteer_type_correct = "i"
-        while volunteer_type_correct != "y":
-            volunteer_type = int(input(f"What type of volunteer would you like to be 1 = {volunteer_type_array[0]}, 2 = {volunteer_type_array[1]}, 3 = {volunteer_type_array[2]} : 1/2/3 ? "))
-            volunteer_type = volunteer_type - 1
-            while volunteer_type != 0 and volunteer_type != 1 and volunteer_type != 2:
-                print("ERROR")
-                print(f"{volunteer_type+1} is not a valid input!")
-                volunteer_type = int(input(f"What type of volunteer would you like to be 1 = {volunteer_type_array[0]}, 2 = {volunteer_type_array[1]}, 3 = {volunteer_type_array[2]} : 1/2/3 ? "))
-                volunteer_type = volunteer_type - 1
-            volunteer_type_correct = str(input(f"Is {volunteer_type_array[volunteer_type]} your correct input: y/n ? ")).lower()
-            while volunteer_type_correct != "y" and volunteer_type_correct != "n":
-                print("ERROR")
-                print(f"{volunteer_type_correct} is not a valid input!")
-                volunteer_type_correct = str(input(f"Is {volunteer_type_array[volunteer_type]} your correct input: y/n ? ")).lower()
-        volunteer = volunteer_type_array[volunteer_type]
-    else:
-        print("Unkown ERROR")
-        volunteer_sing_up()
-    return volunteer
-    #asks user if they would like to be a volunteer
-    #validates input , allows for invalid inputs
-    #if user doesnt want to be volunteer varible is set
-    #if user does, asks type
-    #valdates type, allows for invlaid inputs
-    #volunteer varible is set to type
-    #returns volunteer
-
-def pay_sing_up():
-    pay_today = str(input("Would you like to pay today: y/n ? ")).lower()
-    while pay_today != "y" and pay_today != "n":
-        print("ERROR")
-        print(f"{pay_today} is not a balid input!")
-        pay_today = str(input("Would you like to pay today: y/n ? ")).lower()
-    if pay_today == "y":
-        print("You have been charged $75")
-        payed = "yes"
-    else:
-        payed = "no"
-    return payed
-    #akss user to pay
-    #charges fee if y
-    #doesnt if n
-    #returns payed
-
-def member_sign_up():
-    full_name = fullname_sing_up()
-    date = get_current_date()
-    volunteer = volunteer_sing_up()
-    payed = pay_sing_up()
-    rows = [[full_name, volunteer, date, payed, payed],]
-    filename = "member_records.csv"
-    with open(filename, 'a') as csvfile:
-        csvwriter = csv.writer(csvfile) 
-        csvwriter.writerows(rows)
-    #gets all user info
-    #puts it into an array
-    #appends it to csv file
-
-def sponsor_sign_up():
-    sponsor = str(input("Would you like to become a sponsor a brass plack: y/n ? ")).lower()
-    while sponsor != "y" and sponsor != "n":
-        print("ERROR")
-        print(f"{sponsor} is not a valid input!")
-        sponsor = str(input("Would you like to become a sponsor: y/n ? ")).lower()
-    if sponsor == "y":
-        full_name = fullname_sing_up()
-        sponsor_message_correct = "i"
-        while sponsor_message_correct != "y":
-            sponsor_message = str(input("What message would you like to be on sponsored plack? "))
-            sponsor_message_correct = str(input(f"Is '{sponsor_message}' correct: y/n ? ")).lower()
-            while sponsor_message_correct != "y" and sponsor_message_correct != "n":
-                print("ERROR")
-                print(f"{sponsor_message_correct} is not a valid input!")
-                sponsor_message_correct = str(input(f"Is '{sponsor_message}' correct: y/n ? ")).lower()
-        rows = [[full_name, sponsor_message],]
-        filename = "sponsor_records.csv"
-        with open(filename, 'a') as csvfile:
-            csvwriter = csv.writer(csvfile) 
-            csvwriter.writerows(rows)
-        print("You have been charged $200")
-    elif sponsor == "n":
-        pass
-    else:
-        print("Unkown ERROR")
-        sponsor_sign_up()
-    #asks sponsor to reinput thier name in case they want to sposnor it diffrenlty
-    #asks for message
-    #validates messaage
-    #saves to file
-
-
-
-member_sign_up()
-sponsor_sign_up()
+search_members()
